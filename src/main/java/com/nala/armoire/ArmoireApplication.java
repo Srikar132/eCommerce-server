1850f371d665f6b8b46443b055dd2e7386e84ec4
unknown <bunnyking828@gmail.com>
2025-12-03 23:59:09 +0530

    first commit

---
+++ src/main/java/com/nala/armoire/ArmoireApplication.java
@@ -0,0 +1,13 @@
+package com.nala.armoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmoireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmoireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nala/armoire/ArmoireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nala.armoire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmoireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
